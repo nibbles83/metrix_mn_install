183ccffa4490b7f950c6b44567ac1ce271810b35
nibbles83 <hardke01@yahoo.co.uk>
2020-01-29 09:39:00 +0000

    Inital commit

---
+++ README.md
@@ -0,0 +1,115 @@
+# Metrix Masternode Installation Guide
+
+Metrix masternodes can be configured in a Local "Hot" or local & remote "Hot/Cold" setup.
+
+Hot/Cold setups generally provide better security if configured correctly, although can be more complex to setup for non technical users.
+
+This guide will cover the configuration of both methods.
+
+
+### [Local "Hot" Masternode](#local-masternode)
+
+### [Local & Remote "Hot/Cold" Masternode](#hot-cold-masternode)
+
+
+## Masternode Collateral Tiers
+
+Metrix has multiple tiers of masternodes depending on the amount of coins you have to use as collateral. The rewards percentage is the same for all tiers.
+
+The tier sizes are (in millions) **2, 5, 25. 50, 100**.
+
+The reward for a 2 million node is 3200. A 5 million node is 2.5 times the size, so the reward is 8000.
+
+In all cases of node size the coins must be sent to your local masternode wallet address in a single transaction. The wallet can then identifiy these coins for use as a masternode collateral.
+
+
+## Local Masternode
+
+A local masternode is self contained on your own home PC. To operate a masternode in this way requires that the PC is left running 24/7, masternodes also require a stable internet connection, so wireless network connectivity is not generally recommended.
+
+### System Requirements
+==========================
+
+Most modern day PC's can run a local masternode. A general guide is to ensure you have approx 1GB memory and 10GB free HDD space. This is alongside any other tasks your PC may be performing.
+
+Ensure your wallet is fully up-to-date [Latest Altitiude](https://github.com/TheLindaProjectInc/Altitude/releases/latest).  
+Also ensure thet the core version is updated to the latest, go to **Help > Check for Metrix Core Update**.
+
+Also ensure that your blockheight is fully in sync with the chain [Explorer](https://www.mystakingwallet.com/app/explorer).
+
+For a node to start you need to ensure that other nodes and peers can communicate with yours. This requires that port 33820 is open externally. [Check here](https://www.yougetsignal.com/tools/open-ports/).  
+Put your external IP address and port 33820 to test if the port is open.  
+Assuming its closed you will need to consult your router/firewall documentations to ensure that this port is formwarded to your local PC.
+
+
+### Funding the masternode
+==========================
+
+In Altitude click **Dashboard > Add Account**  
+Choose a label for your masternode "MN1", then click **Create Account**  
+Your new masternode label will appear in the main dashboard. Click that account and copy the address.
+
+Send the full collateral amount to your MN1 address in the **Send** tab.  
+
+Once the amount has been sent you now need to wait for 15 confirmations.
+
+### Setup the masternode
+==========================
+
+Click **Masternodes > Setup Local masternode** in Altitude.  
+The masternode collateral anmount should be detected and available to start as a local masternode.  
+
+Click **Start Local**  
+
+You masternode will then start after a few seconds.
+
+The machine will need to remain running at all times for the masternode to work.
+
+## Hot Cold Masternode
+
+Hot/cold masternodes are made up of a cold wallet that stores your masternode collaterals, alongside a remote wallet containing no coins. This remote wallet needs to remain on at all times and advertises to the network that you have the collateral required.  
+
+Often the remote side is created on a cloud VPS provider. This generally provides better uptime of the node and more security as the coins only exist on the ofline side of the configuration.  
+
+The most common servicesmused with our wallets are [Digital Ocean](https://www.digitalocean.com/) and [Vultr](https://www.vultr.com/).  
+
+We also recommend you do not use elastic cloud services like AWS or Google Cloud for your masternode. These services generally require advanced network knowledge to correctly configure.
+
+### Funding the masternode
+==========================
+
+In Altitude click **Dashboard > Add Account**  
+Choose a label for your masternode "MN1", then click **Create Account**  
+Your new masternode label will appear in the main dashboard. Your cold wallet can control multiple masternodes of varying collateral sizes. For each masternode create a new label/address pair. You will also need a remote VPS per masternode.
+
+Send the full collateral amount to your MN1 address in the **Send** tab.  
+
+Once the amount has been sent you now need to wait for 15 confirmations.
+
+### Setup the masternode
+==========================
+
+Generate your Masternode Private Key
+
+In your wallet, open Tools -> Debug console and run the following command to get your masternode key:
+
+`masternode genkey`
+
+Please note: If you plan to set up more than one masternode, you need to create a key with the above command for each one. These keys are not tied to any specific masternode, but each masternode you run requires a unique key.
+
+Run this command to get your output information:
+
+`masternode outputs`
+
+Copy both the key and output information to a text file.
+
+Click **Masternodes > Add Remote Masternode** in Altitude.  
+
+For each masternode fill in the fields.
+```
+Alias = Label  
+Masternode IP:Port = Public IP and port of your VPS.  
+Masternode Private Key = The output from genkey  
+Masternode TRX Hash = Large hash from outputs
+Masternode TRX index = Single digit from outputs
+```
---
+++ install.sh
@@ -0,0 +1,338 @@
+#!/bin/bash
+
+# Make installer interactive and select normal mode by default.
+INTERACTIVE="y"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -n|--normal)
+    FAIL2BAN="y"
+    UFW="y"
+    BOOTSTRAP="y"
+    shift
+    ;;
+    -i|--externalip)
+    EXTERNALIP="$2"
+    ARGUMENTIP="y"
+    shift
+    shift
+    ;;
+    --bindip)
+    BINDIP="$2"
+    shift
+    shift
+    ;;
+    -k|--privatekey)
+    KEY="$2"
+    shift
+    shift
+    ;;
+    -f|--fail2ban)
+    FAIL2BAN="y"
+    shift
+    ;;
+    --no-fail2ban)
+    FAIL2BAN="n"
+    shift
+    ;;
+    -u|--ufw)
+    UFW="y"
+    shift
+    ;;
+    --no-ufw)
+    UFW="n"
+    shift
+    ;;
+    -b|--bootstrap)
+    BOOTSTRAP="y"
+    shift
+    ;;
+    --no-bootstrap)
+    BOOTSTRAP="n"
+    shift
+    ;;
+    --no-interaction)
+    INTERACTIVE="n"
+    shift
+    ;;
+    -h|--help)
+    cat << EOL
+
+Metrix Masternode installer arguments:
+
+    -n --normal               : Run installer in normal mode
+    -i --externalip <address> : Public IP address of VPS
+    --bindip <address>        : Internal bind IP to use
+    -k --privatekey <key>     : Private key to use
+    -f --fail2ban             : Install Fail2Ban
+    --no-fail2ban             : Do not install Fail2Ban
+    -u --ufw                  : Install UFW
+    --no-ufw                  : Do not install UFW
+    -b --bootstrap            : Sync node using Bootstrap
+    --no-bootstrap            : Do not use Bootstrap
+    -h --help                 : Display this help text.
+    --no-interaction          : Do not wait for wallet activation.
+
+EOL
+    exit
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+clear
+
+# Make sure curl is installed
+apt-get update
+apt-get install -qqy curl jq
+clear
+
+# These should automatically find the latest version of Metrix
+
+TARBALLURL=$(curl -s https://api.github.com/repos/TheLindaProjectInc/Metrix/releases/latest | grep browser_download_url | grep -e "Metrix-linux-x64" | cut -d '"' -f 4)
+TARBALLNAME=$(curl -s https://api.github.com/repos/TheLindaProjectInc/Metrix/releases/latest | grep browser_download_url | grep -e "Metrix-linux-x64" | cut -d '"' -f 4 | cut -d "/" -f 9)
+BOOTSTRAPURL=$(curl -s https://lindaproject.nyc3.digitaloceanspaces.com/metrix/bootstrap/metrix.zip)
+BOOTSTRAPARCHIVE="metrix.zip"
+
+#!/bin/bash
+
+# Check if we are root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
+
+# Check if we have enough memory
+if [[ $(free -m | awk '/^Mem:/{print $2}') -lt 850 ]]; then
+  echo "This installation requires at least 1GB of RAM.";
+  exit 1
+fi
+
+# Check if we have enough disk space
+if [[ $(df -k --output=avail / | tail -n1) -lt 10485760 ]]; then
+  echo "This installation requires at least 10GB of free disk space.";
+  exit 1
+fi
+
+# Install tools for dig and systemctl
+echo "Preparing installation..."
+apt-get install git dnsutils systemd -y > /dev/null 2>&1
+
+# Check for systemd
+systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 16.04?"  >&2; exit 1; }
+
+# Get our current IP
+IPV4=$(dig +short myip.opendns.com @resolver1.opendns.com)
+if [[ $IPV4 == *"connection timed out"* ]]; then
+        IPV4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com| tr -d \")
+fi
+IPV6=$(dig +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com)
+if [[ $IPV6 == *"connection timed out"* ]]; then
+        IPV6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com| tr -d \")
+fi
+if [ -z "$EXTERNALIP" ]; then
+  if [ -n "$IPV4" ]; then
+    EXTERNALIP="$IPV4"
+  else
+    EXTERNALIP="$IPV6"
+  fi
+fi
+clear
+
+if [[ $INTERACTIVE = "y" ]]; then
+echo "
+Metrix Masternode Installer
+"
+
+sleep 3
+fi
+
+USER=root
+
+if [ -z "$FAIL2BAN" ]; then
+  FAIL2BAN="y"
+fi
+if [ -z "$UFW" ]; then
+  UFW="y"
+fi
+if [ -z "$BOOTSTRAP" ]; then
+  BOOTSTRAP="y"
+fi
+INSTALLERUSED="#Used Basic Install"
+
+USERHOME=$(eval echo "~$USER")
+
+if [ -z "$ARGUMENTIP" ]; then
+  read -erp "Server IP Address: " -i "$EXTERNALIP" -e EXTERNALIP
+fi
+
+if [ -z "$BINDIP" ]; then
+    BINDIP=$EXTERNALIP;
+fi
+
+if [ -z "$KEY" ]; then
+  read -erp "Masternode Private Key (e.g. 7edfjLCUzGczZi3JQw8GHp434R9kNY33eFyMGeKRymkB56G4324h # THE KEY YOU GENERATED EARLIER) : " KEY
+fi
+
+if [ -z "$FAIL2BAN" ]; then
+  read -erp "Install Fail2ban? [Y/n] : " FAIL2BAN
+fi
+
+if [ -z "$UFW" ]; then
+  read -erp "Install UFW and configure ports? [Y/n] : " UFW
+fi
+
+if [ -z "$BOOTSTRAP" ]; then
+  read -erp "Do you want to use our bootstrap file to speed the syncing process? [Y/n] : " BOOTSTRAP
+fi
+
+clear
+
+# Generate random passwords
+RPCUSER=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+# update packages and upgrade Ubuntu
+echo "Installing dependencies..."
+apt-get -qq update
+apt-get -qq upgrade
+apt-get -qq autoremove
+apt-get -qq install wget htop xz-utils
+apt-get -qq install git && apt-get -qq install pkg-config
+
+# Install Fail2Ban
+if [[ ("$FAIL2BAN" == "y" || "$FAIL2BAN" == "Y" || "$FAIL2BAN" == "") ]]; then
+  aptitude -y -q install fail2ban
+  # Reduce Fail2Ban memory usage - http://hacksnsnacks.com/snippets/reduce-fail2ban-memory-usage/
+  echo "ulimit -s 256" | sudo tee -a /etc/default/fail2ban
+  service fail2ban restart
+fi
+
+# Install UFW
+if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
+  apt-get -qq install ufw
+  ufw default deny incoming
+  ufw default allow outgoing
+  ufw allow ssh
+  ufw allow 33820/tcp
+  yes | ufw enable
+fi
+
+# Install Metrix daemon
+wget "$TARBALLURL"
+tar -xzvf "$TARBALLNAME" -C /usr/local/bin
+rm "$TARBALLNAME"
+
+# Create .Metrixcore directory
+mkdir "$USERHOME/.metrix"
+
+# Install bootstrap file
+if [[ ("$BOOTSTRAP" == "y" || "$BOOTSTRAP" == "Y" || "$BOOTSTRAP" == "") ]]; then
+  echo "Installing bootstrap file..."
+  wget "$BOOTSTRAPURL" && unzip $BOOTSTRAPARCHIVE -d "$USERHOME/.metrix/" && rm $BOOTSTRAPARCHIVE
+fi
+
+# Create Metrix.conf
+touch "$USERHOME/.metrix/metrix.conf"
+
+cat > "$USERHOME/.metrix/metrix.conf" << EOL
+${INSTALLERUSED}
+bind=[${BINDIP}]:52543
+daemon=1
+externalip=[${EXTERNALIP}]
+listen=1
+logtimestamps=1
+masternode=1
+masternodeaddr=[${EXTERNALIP}]
+masternodeprivkey=${KEY}
+maxconnections=256
+rpcallowip=127.0.0.1
+rpcpassword=${RPCPASSWORD}
+rpcuser=${RPCUSER}
+server=1
+EOL
+fi
+chmod 0600 "$USERHOME/.metrix/metrix.conf"
+chown -R $USER:$USER "$USERHOME/.metrix"
+
+sleep 1
+
+cat > /etc/systemd/system/metrixd.service << EOL
+[Unit]
+Description=Metrix's distributed currency daemon
+After=network-online.target
+[Service]
+Type=forking
+User=${USER}
+WorkingDirectory=${USERHOME}
+ExecStart=/usr/local/bin/metrixd -conf=${USERHOME}/.metrix/metrix.conf -datadir=${USERHOME}/.metrix
+ExecStop=/usr/local/bin/metrix-cli -conf=${USERHOME}/.metrix/metrix.conf -datadir=${USERHOME}/.metrix stop
+Restart=on-failure
+RestartSec=1m
+StartLimitIntervalSec=5m
+StartLimitInterval=5m
+StartLimitBurst=3
+[Install]
+WantedBy=multi-user.target
+EOL
+systemctl enable metrixd
+echo "Starting Metrixd, will check status in 60 seconds..."
+systemctl start metrixd
+
+sleep 60
+
+if ! systemctl status metrixd | grep -q "active (running)"; then
+  echo "ERROR: Failed to start metrixd. Please contact support."
+  exit
+fi
+
+echo "Waiting for wallet to load..."
+until su -c "Metrix-cli getinfo 2>/dev/null | grep -q \"version\"" $USER; do
+  sleep 1;
+done
+
+clear
+
+cat << EOL
+
+  Your blockchain is now syncing. This can take up to an hour to complete.
+
+  When the chain is in sync, you need to start your masternode! 
+  If you haven't already, please add this node to your masternode.conf file. Once you have 
+  added the node, restart and unlock your Altitude wallet! Then, go 
+  to the Masternodes tab, and click "Start ALL."
+
+  To check the sync status of this node
+
+  metrix-cli getinfo
+
+  When fully in sync and the node is started from altitude
+
+  metrix-cli masternode status
+
+EOL
+
+
+#if [[ $INTERACTIVE = "y" ]]; then
+#  read -rp "Press Enter to continue after you've started the node in your wallet. " -n1 -s
+#fi
+
+#clear
+
+#sleep 1
+#su -c "/usr/local/bin/metrix-cli masternode local false" $USER
+#sleep 1
+#cleara
+#su -c "/usr/local/bin/metrix-cli masternode status" $USER
+#sleep 5
+
+echo "" && echo "Masternode setup completed!" 
+echo ""
